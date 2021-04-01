aef8f6a66e716373ff0d6a0a795f745015205b9c
Sinkule Josef <819817@stma.k12.mn.us>
2021-04-01 10:02:33 -0500

    close10

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def close10(x, y):
    if abs(10-y) < abs(10-y):
       return(x)
-   elif abs(10-x) == (10-y):
+   elif abs(10-x) == abs(10-y):
       return(0)
    else:
       return(y)
 
-print(close10(10,3))
+print(close10(5,12))
